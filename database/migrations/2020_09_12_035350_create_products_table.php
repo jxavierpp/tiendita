7cb6f1d8aa1878f959ea2ddaa578ff54b9c906c6
Xavier Paredes <jxavierpp1@gmail.com>
2020-09-12 03:16:39 -0700

    Se agregaron las migraciones

---
+++ database/migrations/2020_09_12_035350_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('inventory');
+            $table->float('cost', 8, 2);
+            $table->boolean('availability');
+            $table->foreignId('category_id')
+                ->nullable()
+                ->references('id')->on('categories')
+                ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
