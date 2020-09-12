ed02372da491de80e3f2337e1e602ddb23400b75
Xavier Paredes <jxavierpp1@gmail.com>
2020-09-12 03:16:21 -0700

    se agregaron las rutas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rutas de las Categorias
+Route::get('/','CategoryController@index');
+
+Route::get('/categories','CategoryController@index');
+Route::get('/categories/create', 'CategoryController@create');
+Route::post('/categories/store', 'CategoryController@store');
+Route::get('/categories/edit/{id}', 'CategoryController@edit');
+Route::put('/categories/{id}', 'CategoryController@update');
+Route::delete('/categories/{id}', 'CategoryController@destroy');
+
+// Rutas de los Productos
+Route::get('/products','ProductController@index');
+Route::get('/products/create', 'ProductController@create');
+Route::post('/products/store', 'ProductController@store');
+Route::get('/products/edit/{id}', 'ProductController@edit');
+Route::put('/products/{id}', 'ProductController@update');
+Route::delete('/products/{id}', 'ProductController@destroy');
