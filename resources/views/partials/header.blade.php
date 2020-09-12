0a12b260ce3ba064fceeabb006bb3ac74ffe9490
Xavier Paredes <jxavierpp1@gmail.com>
2020-09-12 03:19:04 -0700

    se agregaron los partials para la vista

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>@yield('titulo')</title>
+
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<!-- Custom Fonts -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet'
+      type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+      rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+ <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,10 @@
+<nav id="sidebar">
+    <div class="sidebar-header">
+        <h3>Mi Tiendita</h3>
+    </div>
+
+    <ul class="list-unstyled components">
+        <li><a href="/categories">Lista de Categorias</a></li>
+        <li><a href="/products">Lista de Productos</a></li>
+    </ul>
+</nav>
